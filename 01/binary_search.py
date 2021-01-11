de23daacc173d216c7d5ac1252742d04278449ae
Rhange <rhange0511@gmail.com>
2021-01-11 13:15:12 +0900

    Updated Ch.1

---
+++ 01/binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(list, item):
+    low = 0     # 최소값
+    high = len(list) - 1    # 최대값
+
+    while low <= high:
+        mid = (low + high) // 2     # 중간 인덱스, 중간부터 시작
+        guess = list[mid]   # 중간 인덱스의 값 확인
+
+        if guess == item:   # 목표값과 같다면
+            return mid  # 해당 인덱스 값을 리턴
+        if guess > item:    # 추측한 값이 목표값보다 크다면
+            high = mid - 1  # 최대값을 중간값보다 작게 설정
+        else:
+            low = mid + 1   # 추측한 값이 목표보다 작다면 최소값을 중간값 + 1로 설정
+    return None
+
+
+# Test
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 3))
+print(binary_search(my_list, -1))
